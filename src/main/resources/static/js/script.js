65a9006d38b0cf9037b9666102840e3ffe0ceecc
Kharanshu Wanare <kharanshuw@gmail.com>
2025-03-18 00:06:35 +0530

    implemented theme switcher

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,60 @@
+console.log("Theme Switcher Script Loaded");
+console.log("theme stored in localStorage is ", localStorage.getItem('theme'));
+
+// Get the theme from localStorage or default to "light"
+let currentTheme = localStorage.getItem("theme") || "light";
+
+document.querySelector('html').setAttribute('class', "");
+
+// Apply the initial theme
+document.querySelector('html').classList.add(currentTheme);
+
+function toggleTheme() {
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+
+    document.querySelector('html').classList.remove(currentTheme);
+
+    document.querySelector('html').classList.add(newTheme);
+
+    localStorage.setItem("theme", newTheme);
+
+    currentTheme = newTheme;
+
+    updateButtonText();
+}
+
+
+function updateButtonText() {
+    const themeButton = document.querySelector("#theme_change_button_span");
+
+    if (themeButton) {
+        console.log("themeButton selected successfully ", themeButton);
+
+        if (currentTheme === "light") {
+            themeButton.textContent = "Dark Mode";
+        } else {
+            themeButton.textContent = "Light Mode";
+        }
+    }
+}
+
+
+
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        const themeButton = document.querySelector("#theme_change_button");
+
+        if (themeButton) {
+            console.log("themeButton successfully selected in addEventListener", themeButton);
+
+            themeButton.addEventListener("click", toggleTheme);
+
+            updateButtonText();
+
+        }
+
+        else {
+            console.log("Theme toggle button not found. Make sure you have an element with id='themeToggle' in your HTML");
+        }
+    }
+)
