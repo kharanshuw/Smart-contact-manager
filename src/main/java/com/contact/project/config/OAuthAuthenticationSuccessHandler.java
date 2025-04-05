acf14c94372a57cc67079dabe92b124ab165bf38
Kharanshu Wanare <kharanshuw@gmail.com>
2025-04-05 13:27:55 +0530

    added login using google and github

--- src/main/java/com/contact/project/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/contact/project/config/OAuthAuthenticationSuccessHandler.java
@@ -144,6 +144,8 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 
             user.setProviderUserId(providerId);
 
+            user.setEmailVerified(true);
+
         }
 
         else {
